6e031365cb9793754f1d6a72736d65de2b28d6ec
Sants Yonezu <51303087+yon3zu@users.noreply.github.com>
2025-03-25 20:31:03 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
       return [
         {
           source: '/api/fetch-domains',
-          destination: 'https://api.xreverselabs.org/api/discover_domain?apiKey=DiscoverDomain',
+          destination: 'https://api.xreverselabs.org/api/discover_domain?apiKey=b2c442f36a149dde9a1542c2e4d1a3ea',
         },
       ];
     },
